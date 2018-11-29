51c87b6c52e06418449087a74044e029a47090c5
thomas_lddv <thomas.lddv@gmail.com>
2018-11-29 11:26:10 +0100

    cycle detector fonctionne, avec main test

--- level_5/cycle_detector/cycle_detector.c
+++ level_5/cycle_detector/cycle_detector.c
@@ -2,14 +2,45 @@
 
 int		cycle_detector(const t_list *list)
 {
-	t_list *next;
+	t_list *comp;
+	t_list *comp2;
 
-	next = list->next;
-	while (next)
+	comp = (t_list *)list;
+	comp2 = comp;
+	while (comp2 && comp2->next)
 	{
-		if (next == list)
+		comp = comp->next;
+		comp2 = comp2->next->next;
+		if (comp == comp2)
 			return (1);
-		next = next->next;
 	}
+	return (0);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+t_list	*lstnew(int data)
+{
+	t_list *new;
+
+	if(!(new = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	new->next = NULL;
+	new->data = data;
+	return (new);
+}
+
+int		main(void)
+{
+	t_list *list;
+
+	list = lstnew(0);
+	list->next = lstnew(5);
+	list->next = list;
+	/*list->next->next = lstnew(10);
+	list->next->next->next = lstnew(15);
+	list->next->next = list->next;
+	*/printf("%d\n", cycle_detector(list));
 	return (0);
 }
