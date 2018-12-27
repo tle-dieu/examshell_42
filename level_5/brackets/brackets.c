cc2781d20ad6fef9d093989f8aaeb354bc879c5e
tle-dieu <thomas.lddv@orange.fr>
2018-12-27 01:05:51 +0100

    bracket up

--- level_5/brackets/brackets.c
+++ level_5/brackets/brackets.c
@@ -1,10 +1,60 @@
-void	brackets(char const *s)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   brackets.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/18 10:03:58 by exam              #+#    #+#             */
+/*   Updated: 2018/12/27 00:56:35 by thomas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		brackets(char *s)
+{
+	int bracket;
+	char *last;
+
+	last = s;
+	bracket = 0;
+	while (*s)
+	{
+		if (*s == '(' || *s == '{' || *s == '[')
+		{
+			if (*s == '(')
+				*last++ = ')';
+			else if (*s == '{')
+				*last++ = '}';
+			else
+				*last++ = ']';
+			bracket++;
+		}
+		else if (*s == ')' || *s == '}' || *s == ']')
+			if (!bracket-- || *--last != *s)
+				return (1);
+		s++;
+	}
+	return (bracket);
+}
+
+int		main(int ac, char **av)
 {
 	int i;
 
-	i = 0;
-	while (s[i])
+	if (ac >= 2)
 	{
-		i++;
+		i = 1;
+		while (i < ac)
+		{
+			if (brackets(av[i++]))
+				write(1, "Error\n", 6);
+			else
+				write(1, "OK\n", 3);
+		}
 	}
+	else
+		write(1, "\n", 1);
+	return (0);
 }
--- level_5/brackets/brackets.h
+++
@@ -1,9 +0,0 @@
-#ifndef BRACKETS_H
-# define BRACKETS_H
-
-void	brackets(char const *s);
-void	ft_putchar(char c);
-void	ft_putstr(char const *s);
-void	ft_putendl(char const *s);
-
-#endif
